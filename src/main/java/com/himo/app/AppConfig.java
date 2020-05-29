50579126845a8e2eff8ec25609c2594d86949c83
Jonas <jonas.weigt42@gmail.com>
2020-05-29 15:44:38 +0200

    added hoses website

--- src/main/java/com/himo/app/AppConfig.java
+++ src/main/java/com/himo/app/AppConfig.java
@@ -1,11 +1,17 @@
 package com.himo.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.himo.app.view.MainView;
+
 @Configuration
+@EnableScheduling
 public class AppConfig
 {
 
@@ -13,5 +19,11 @@ public class AppConfig
 	public PasswordEncoder passwordEncoder()
 	{
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public Logger myLogger()
+	{
+		return LoggerFactory.getLogger(MainView.class);
 	}
 }
--- src/main/java/com/himo/app/view/MainView.java
+++ src/main/java/com/himo/app/view/MainView.java
@@ -3,12 +3,32 @@ package com.himo.app.view;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import com.himo.app.component.Login;
 import com.himo.app.component.Logo;
@@ -59,7 +79,12 @@ public class MainView extends AppLayout
 
 	@Autowired
 	private Login login;
+
+	@Autowired
+	private Logger logger;
 	
+	private int zahl = 5;
+
 	@PostConstruct
 	public void init() throws IOException, URISyntaxException
 	{
@@ -70,6 +95,230 @@ public class MainView extends AppLayout
 		menu = createMenuTabs();
 		addToDrawer(menu);
 		login.open();
+	}
+
+	@Scheduled(fixedRate = 300000)
+	public void hoereAufHosesSeite()
+	{
+		logger.info("-------started------job----------");
+		try
+		{
+			WebDriver driver;
+
+			System.setProperty("webdriver.chrome.driver", "chromedriver");
+
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");
+
+			driver = new ChromeDriver(options);
+			
+			driver.get("https://netappoint.de/onlineticket-25/index.php?company=kreis-ostholstein&cur_cause=0");
+
+			klickFahrzeugZulassung(driver);
+			selektiere1xFahrzeugZulassung(driver);
+			klickWeiter(driver);
+			
+			boolean terminImJuni = istImJuniWasFrei(driver);
+
+			if(terminImJuni)
+			{
+				Thread.sleep(2000);
+				Set<Integer> vergebeneTermine = new HashSet<>();
+				Set<Integer> freieTermine = new HashSet<>();
+				
+				findeFreieTermine(driver, vergebeneTermine, freieTermine);
+				
+				for(Integer i : vergebeneTermine)
+				{
+					logger.info("freie Termine für den " + i + ". gefunden");
+				}
+				if (!freieTermine.isEmpty())
+				{
+					sendMail(freieTermine);
+				}
+			}
+			
+			driver.close();
+
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+			sendMailJonas(e.getMessage());
+			logger.error("-------finished---job---with---errors-------", e);
+		}
+		logger.info("nicht vergessen, bei google die mailconfig wieder zu ändern! von unsicheren quellen mails senden");
+		logger.info("-------finished------job----------");
+	}
+
+	public void findeFreieTermine(WebDriver driver, Set<Integer> logSet, Set<Integer> set)
+	{
+		List<WebElement> list4 = driver.findElements(By.className("nat_calendar_weekday_bookable"));
+		for (WebElement element : list4)
+		{
+			int day = Integer.parseInt(element.getText());
+			logSet.add(day);
+			if (day < zahl)
+			{
+				set.add(day);
+				zahl = day;
+			}
+		}
+	}
+
+	public boolean istImJuniWasFrei(WebDriver driver)
+	{
+		List<WebElement> list3 = driver.findElements(By.tagName("abbr"));
+		for(WebElement element : list3)
+		{
+			if(element.getText().contains("Mai"))
+			{
+				WebElement clickNextMonth = driver.findElement(By.className("nat_navigation_button_img"));
+				clickNextMonth.click();
+				logger.info("zum Juni gewechselt");
+				break;
+			}
+			if(element.getText().contains("Juli"))
+			{
+				logger.info("Nur Termine im Juli frei...");
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void klickWeiter(WebDriver driver)
+	{
+		List<WebElement> list2 = driver.findElements(By.tagName("input"));
+		for (WebElement element : list2)
+		{
+			if (element.getAttribute("value").equals("Weiter"))
+			{
+				element.click();
+				logger.info("Weiter geklickt");
+			}
+		}
+	}
+
+	public void selektiere1xFahrzeugZulassung(WebDriver driver)
+	{
+		Select zulassungNeuFahrzeige = new Select(driver.findElement(By.id("casetype_1013")));
+		if (zulassungNeuFahrzeige != null)
+		{
+			zulassungNeuFahrzeige.selectByValue("1");
+			logger.info("1x Fahrzeugzulassung ausgewählt");
+		}
+	}
+
+	public void klickFahrzeugZulassung(WebDriver driver)
+	{
+		logger.info("schaue für Termine vor dem " + zahl + ". Juni");
+		List<WebElement> list = driver.findElements(By.tagName("H3"));
+		for (WebElement element : list)
+		{
+			if (element.getText().equals("Fahrzeugzulassung"))
+			{
+				element.click();
+				logger.info("Fahrzeugzulassung geklickt");
+			}
+		}
+	}
+
+	private void sendMail(Set<Integer> set)
+	{
+		final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+		// Get a Properties object
+		Properties props = System.getProperties();
+		props.setProperty("mail.smtp.host", "smtp.gmail.com");
+		props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+		props.setProperty("mail.smtp.socketFactory.fallback", "false");
+		props.setProperty("mail.smtp.port", "465");
+		props.setProperty("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.debug", "true");
+		props.put("mail.store.protocol", "pop3");
+		props.put("mail.transport.protocol", "smtp");
+		final String username = "jonas.weigt42@gmail.com";
+		final String password = "Rastafari.142857";
+		try
+		{
+			Session session = Session.getInstance(props, new Authenticator()
+			{
+				protected PasswordAuthentication getPasswordAuthentication()
+				{
+					return new PasswordAuthentication(username, password);
+				}
+			});
+
+			// -- Create a new message --
+			Message msg = new MimeMessage(session);
+
+			// -- Set the FROM and TO fields --
+			msg.setFrom(new InternetAddress("jonas.weigt42@gmail.com"));
+			msg.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse("hosenthienmarcel@gmail.com,jonas.weigt42@gmail.com", false));
+			msg.setSubject("Breaking News");
+
+			String numbers = "";
+
+			for (Integer i : set)
+			{
+				numbers = numbers.concat("\n" + i + ". Juni");
+			}
+			msg.setText("Griaß di Häuptling,\n am " + numbers
+					+ " ist ein Termin frei geworden. Schnapp zu wie eine Viper! Grüße, Sigis Code\n" 
+					+ "Hier der Link: https://netappoint.de/onlineticket-25/index.php?company=kreis-ostholstein&cur_cause=0 ");
+			msg.setSentDate(new Date());
+			Transport.send(msg);
+			logger.info("Message sent.");
+		} catch (MessagingException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	private void sendMailJonas(String text)
+	{
+		final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+		// Get a Properties object
+		Properties props = System.getProperties();
+		props.setProperty("mail.smtp.host", "smtp.gmail.com");
+		props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+		props.setProperty("mail.smtp.socketFactory.fallback", "false");
+		props.setProperty("mail.smtp.port", "465");
+		props.setProperty("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.debug", "true");
+		props.put("mail.store.protocol", "pop3");
+		props.put("mail.transport.protocol", "smtp");
+		final String username = "jonas.weigt42@gmail.com";
+		final String password = "Rastafari.142857";
+		try
+		{
+			Session session = Session.getInstance(props, new Authenticator()
+			{
+				protected PasswordAuthentication getPasswordAuthentication()
+				{
+					return new PasswordAuthentication(username, password);
+				}
+			});
+
+			// -- Create a new message --
+			Message msg = new MimeMessage(session);
+
+			// -- Set the FROM and TO fields --
+			msg.setFrom(new InternetAddress("jonas.weigt42@gmail.com"));
+			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("jonas.weigt42@gmail.com", false));
+			msg.setSubject("Exception thrown :( ");
+			msg.setText(text);
+			msg.setSentDate(new Date());
+			Transport.send(msg);
+			logger.info("Message sent.");
+		} catch (MessagingException e)
+		{
+			e.printStackTrace();
+		}
+
 	}
 
 	private Tabs createMenuTabs()
@@ -127,5 +376,5 @@ public class MainView extends AppLayout
 		}).findFirst();
 		tabToSelect.ifPresent(tab -> menu.setSelectedTab((Tab) tab));
 	}
-	
+
 }
